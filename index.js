f1b214058df96e0139a3f4d34546e87527e0f7c3
RegalAdhikari <regalbrt@gmail.com>
2022-10-27 00:52:04 +0545

    keys work

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ var but;
 var audio;
 for (i=0;i<7;i++)
 {
-document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
-function handleClick()
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    var click = this.innerHTML;
+    handleClick(click);
+});
+document.addEventListener("keydown",function(event)
 {
-    but = this.innerHTML;
+    
+    handleClick(event.key);
+    console.log(event.key);
+});
+function handleClick(but)
+{
+  
     switch (but) {
         case "w":
             audio = new Audio("sounds/tom-1.mp3");
             audio.play();
             break;
     
-            case "a":
+            case "w":
                 audio = new Audio("sounds/tom-2.mp3")
                 audio.play();
                 break;
