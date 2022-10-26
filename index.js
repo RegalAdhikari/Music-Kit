d7ede582d2802276d7d2c633ba5b8f378fc74b6f
RegalAdhikari <regalbrt@gmail.com>
2022-10-27 00:15:54 +0545

    drum functioning

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
+var but;
+var audio;
 for (i=0;i<7;i++)
 {
-document.querySelectorAll("button")[i].addEventListener("click",handleClick)
+document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
 function handleClick()
 {
-    alert(i+"th button clicked!!");
+    but = this.innerHTML;
+    switch (but) {
+        case "w":
+            audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+    
+            case "a":
+                audio = new Audio("sounds/tom-2.mp3")
+                audio.play();
+                break;
+            case "a":
+                audio = new Audio("sounds/tom-2.mp3")
+                audio.play();
+                break;
+            case "s":
+                audio = new Audio("sounds/tom-3.mp3")
+                audio.play();
+                break;
+            case "d":
+                audio = new Audio("sounds/tom-4.mp3")
+                audio.play();
+                break;
+            case "j":
+                audio = new Audio("sounds/crash.mp3")
+                audio.play();
+                break;
+            case "k":
+                audio = new Audio("sounds/kick-bass.mp3")
+                audio.play();
+                break;
+             case "l":
+                audio = new Audio("sounds/snare.mp3")
+                audio.play();
+                break;
+        default:
+            console.log(this.innerHTML);
+            break;
+    }
+    
+    
     
 }
 }
